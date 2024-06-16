769d5e66cee981d08af1010098209572498bcef3
atleet987 <tripathiabhay261@gmail.com>
2024-06-16 11:51:48 +0530

    Initial Work

--- components/Features/featuresData.tsx
+++ components/Features/featuresData.tsx
@@ -3,46 +3,53 @@ import { Feature } from "@/types/feature";
 const featuresData: Feature[] = [
   {
     id: 1,
-    icon: "/images/icon/icon-01.svg",
-    title: "Crafted for SaaS",
+    icon: "/images/logo/anc_logo2.png",
+    title: "Academics Wing",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
+      "The link between students and the institute's policy makers which ensures that a student's academic life at IITK runs smoothly.",
   },
   {
     id: 2,
-    icon: "/images/icon/icon-02.svg",
-    title: "High-quality Design",
+    icon: "/images/logo/anc_logo2.png",
+    title: "Career Development Wing",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
   },
   {
     id: 3,
-    icon: "/images/icon/icon-03.svg",
-    title: "Next.js 13 + TypeScript",
+    icon: "/images/logo/anc_logo2.png",
+    title: "Research Wing",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
   },
   {
     id: 4,
-    icon: "/images/icon/icon-04.svg",
-    title: "Sanity Blog and Docs",
+    icon: "/images/logo/anc_logo2.png",
+    title: "International Relations Wing",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
   },
   {
     id: 5,
-    icon: "/images/icon/icon-05.svg",
-    title: "DB, Auth and Stripe",
+    icon: "/images/logo/anc_logo2.png",
+    title: "Web Wing",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
   },
   {
     id: 6,
-    icon: "/images/icon/icon-06.svg",
-    title: "Regular Free Updates",
+    icon: "/images/logo/anc_logo2.png",
+    title: "Media & Publicity Wing",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
   },
+  {
+    id: 7,
+    icon: "/images/logo/anc_logo2.png",
+    title: "Outreach & Finance Wing",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
+  }
 ];
 
 export default featuresData;
