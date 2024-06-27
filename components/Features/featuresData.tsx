94a148fa81ee0627b81fd26a10fcae093b9530ff
atleet987 <tripathiabhay261@gmail.com>
2024-06-27 14:41:36 +0530

    Added few logos and images & Career Dev page

--- components/Features/featuresData.tsx
+++ components/Features/featuresData.tsx
@@ -3,21 +3,21 @@ import { Feature } from "@/types/feature";
 const featuresData: Feature[] = [
   {
     id: 1,
-    icon: "/images/logo/anc_logo2.png",
+    icon: "/images/logo/UG Acads logo_2024.svg",
     title: "Academics Wing",
     description:
       "The link between students and the institute's policy makers which ensures that a student's academic life at IITK runs smoothly.",
   },
   {
     id: 2,
-    icon: "/images/logo/anc_logo2.png",
+    icon: "/images/logo/CDev_logo.svg",
     title: "Career Development Wing",
     description:
       "The Wing aimed at providing students with providing proper guidance and helping them develop careers in the fields that align with their professional interests.",
   },
   {
     id: 3,
-    icon: "/images/logo/anc_logo2.png",
+    icon: "/images/logo/Rsearch Wing Logo SVG.svg",
     title: "Research Wing",
     description:
       "The Wing dedicated to fostering and encouraging research enthusiasm among students, ultimately helping to cultivate innovators and future leaders.",
