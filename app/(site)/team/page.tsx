7e7b97235a58a25f7f782bdbd767ccaaa47a8902
vaibhav-itauriya <vaibhavitauriya@gmail.com>
2024-07-20 12:34:52 +0530

    Udated Footer and  Features Links and removed arrow sign from FeatureTab

--- app/(site)/team/page.tsx
+++ app/(site)/team/page.tsx
@@ -14,8 +14,8 @@ import {
 } from "@/components/Teams/teamsData";
 
 export const metadata: Metadata = {
-  title: "Next.js Starter Template for SaaS Startups - Solid SaaS Boilerplate",
-  description: "This is Home for Solid Pro",
+  title: "Contact Us",
+  description: "Teams",
 };
 
 const headerInfo = {
