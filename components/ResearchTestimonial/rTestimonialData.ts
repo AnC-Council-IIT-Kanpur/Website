47ee027c5824665f5aa3ef6656db8fb642a2c527
Sachida <navik11.me@gmail.com>
2024-08-02 14:41:19 +0530

    Testimonial bug fixed

--- components/ResearchTestimonial/rTestimonialData.ts
+++ components/ResearchTestimonial/rTestimonialData.ts
@@ -9,6 +9,7 @@ export const rTestimonialData: Testimonial[] = [
     designation: "Y19",
     image: image1,
     intern: "Research Intern @ University of New Brunswick, Canada",
+    link: "",
     content:
       "You should always be eager to learn things you are interested in and never hesitate to seek help from your mates or professors or anyone who is involved in the field.",
   } ,
@@ -18,6 +19,7 @@ export const rTestimonialData: Testimonial[] = [
     designation: "Y19",
     image: image2,
     intern: "Trading Analyst Intern @ Axxela",
+    link: "",
     content:"In Caltech there is greater independence. The bridge between being felt like an undergrad and a researcher becomes thinner with more freedom. When we do an UGP here at IITK there is constant guidance from the mentors. Both the systems have their own pros and cons but working in Caltech was a great experience for me.",
   }
 ];
