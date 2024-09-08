7682a6d8c50b52f875d6134c68efc9dce174f564
Krishnaag23 <krishnaag23@iitk.ac.in>
2024-09-09 01:19:31 +0530

    Removed dummy links to homepage

--- app/(site)/KnowYourFaculty/page.tsx
+++ app/(site)/KnowYourFaculty/page.tsx
@@ -64,7 +64,7 @@ export default function KnowYourFacultyPage() {
         const videos = await fetchPlaylistVideos();
         setPlaylistVideos(videos);
       } catch (error) {
-        setError("Error fetching video data.");
+        
       } finally {
         setLoading(false);
       }
